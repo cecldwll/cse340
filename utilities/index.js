a8fb3ef3b5bb2b12d28f52adf80bdd3d908f9de0
Caitlyn Caldwell <cecldwll@gmail.com>
2025-01-29 00:01:45 -0700

    quick adjustments

--- utilities/index.js
+++ utilities/index.js
@@ -5,19 +5,19 @@ const Util = {} // create an empty Util object
  * Constructs the nav HTML unordered list
  *******************************/
 Util.getNav = async function (req, res, next) { // creates an asynchronous function, which accepts the request, response and next methods as parameters
-    let data =  await invModel.getClassifications() // calls the getClassifications() function and stores the returned resultset into the data variable
-    let list = "" // creates a JavaScript string
-    list += 'Home' //the list variable has an addition string added to what already exists
-    data.rows.forEach((row) => { // uses a forEach loop to move through the rows of the data array
-        list += " " // appends an opening list item to the string in the lsit variable
-        list += // appends the code that is found on lines 14 through 20 as a string to the list variable
-          ' ' + // a string that includes the beginning of an HTML anchor
-          row.classification_name + // classification_name being displayed between the opening and closing HTML
-          " " 
-        list += " " //
-    })
-    list += " "
-    return list
+  let data = await invModel.getClassifications() // calls the getClassifications() function and stores the returned resultset into the data variable
+  let list = "<ul>" // start an unordered list
+  list += '<li><a href="/">Home</a></li>' // add home link
+  
+  data.rows.forEach((row) => { // loop through classifications
+    list += `<li>` // open list item
+    list += `<a href="/classification/${row.classification_id}" title="View our ${row.classification_name} inventory">` // create anchor
+    list += `${row.classification_name}` // add classification name
+    list += `</a></li>` // close anchor and list item
+  })
+  
+  list += "</ul>" // close unordered list
+  return list // return generated html list
 }
 
 module.exports = Util
