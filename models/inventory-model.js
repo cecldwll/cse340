52a3263833f8f9c6e3c5ee7d9ee0aefad795eb05
Caitlyn Caldwell <cecldwll@gmail.com>
2025-02-08 19:35:37 -0700

    even more changes, still no solutions

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -9,23 +9,21 @@ async function getClassifications(){ // creates an asynchronous function that re
     ) // returns the result of the SQL query, which will be sent to the database server using a pool connection
 }
 
-
-/* *******************************
-* Get all inventory items and classification_name by classification_id
-*******************************/
+  /* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
 async function getInventoryByClassificationId(classification_id) { // creates an asynchronous function
-    try {
-      const data = await pool.query( // calls the query() function
-        `SELECT * FROM public.inventory AS i 
-        JOIN public.classification AS c 
-        ON i.classification_id = c.classification_id 
-        WHERE i.classification_id = $1`,
-        [classification_id]
-      );
-      return data.rows; // returns the result of the SQL query
-    } catch (error) {
-      console.error("getclassificationsbyid error " + error);
-    }
+  try {
+    const data = await pool.query( // calls the query() function
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id]
+    )
+    return data.rows // returns the result of the SQL query
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
   }
-
+}
 module.exports = {getClassifications, getInventoryByClassificationId}; // exports the function for use elsewhere
