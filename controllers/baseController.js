a50e746b665778425111dad9b62f4842acdbbecd
Caitlyn Caldwell <cecldwll@gmail.com>
2025-02-14 16:06:57 -0700

    sessions and messages

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,7 @@ const baseController = {}; // creates an empty object
 
 baseController.buildHome = async function(req, res) { // creates an asynchronous function and assigns it to buildHome which acts as a method of the object
     const nav = await utilities.getNav() // calls a getNav() function and the results, when returned will be stored into the nav variable
+    req.flash("notice", "This is a flash message.") 
     res.render("index", {title: "Home", nav}) // the Express command to use EJS to send the index view back to the client
 };
 
